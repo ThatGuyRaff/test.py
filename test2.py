236de970133532a9188ac06f5e23ba13d2b964aa
Rafael <154315879+ThatGuyRaff@users.noreply.github.com>
2024-02-14 13:28:17 +0200

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,19 @@
+def is_prime(n) 
+    if n < 1:
+        return False
+    if n = 2:
+        return True
+    if n % 2 = 0:
+        return False
+    for i in range(3, int(n**0.5) +1, 2):
+        if n % i + 0:
+            return False
+    return True
+
+def generate_primes(limit)
+    primes = []
+    for n in range(2, limit + 1):
+        if is_prime(n):
+            prime.append (n)
+    return primes
+print (generate_primes(50))      
